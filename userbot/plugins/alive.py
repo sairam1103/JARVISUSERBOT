6a9bbedb3b39ba100a00defee918ab823bbe74d7
sairam1103 <65078740+sairam1103@users.noreply.github.com>
2020-07-02 17:04:16 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,18 +8,18 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-FRIDAY_IS_ALIVE = ("**F.R.I.D.A.Y** IS AT YOUR SERVICE❗\n\n"
+JARVIS_IS_ALIVE = ("**J.A.R.VI.S.** IS AT YOUR SERVICE❗\n\n"
                    "**I am Always Alive To Help You Boss**\n\n"
-                   "**♧︎︎︎Currently Status♧︎︎︎** : `No Issue Found`\n\n"
-                   "**♧︎︎︎Current Branch♧︎︎︎** : `master`\n\n"
-                   "**♧︎︎︎Python Version♧︎︎︎** : `3.8`\n\n"
-                   "**♧︎︎︎Friday OS♧︎︎︎** : `3.0 Snapdragon`\n\n"
-                   "**♧︎︎︎Current Sat♧︎︎︎** : `StarkGangSat-2.0`\n\n"
-                   f"**♧︎︎︎My Boss♧︎︎︎** : {DEFAULTUSER} \n\n"
-                   "**♧︎︎︎Heroku Database♧︎︎︎** : `No Known Error Found`\n\n"
-                   "**♧︎︎︎License♧︎︎︎** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-                   "♧︎︎︎Copyright♧︎︎︎ : By [StarkGang@Github](GitHub.com/StarkGang)\n\n"
-                   " [⌨︎Deploy FridayUserbot⌨︎](https://telegra.ph/FRIDAY-06-15)") 
+                   "**❌Currently Status❌** : `No Issue Found`\n\n"
+                   "**❌Current Branch❌** : `master`\n\n"
+                   "**❌Python Version❌** : `3.8`\n\n"
+                   "**❌JARVIS OS❌** : `3.0 Snapdragon `\n\n"
+                   "**❌Current Sat❌** : `SIDDARTHASat-2.0`\n\n"
+                   f"**❌My Boss❌** : {DEFAULTUSER} \n\n"
+                   "**❌Heroku Database❌** : `No Known Error Found`\n\n"
+                   "**❌License❌** : [MIT Licence](https://github.com/sairam1103/JARVISUSERBOT//blob/master/LICENSE)\n\n"
+                   "❌Copyright❌ : By [SAIRAM1103@Github](GitHub.com/SAIRAM1103)\n\n"
+                   " [⌨︎Deploy JarvisUserbot⌨︎](https://telegra.ph/file/e9d5181b2a7dc98973b68.jpg)") 
 
 
 #@command(outgoing=True, pattern="^.alive$")
@@ -28,4 +28,4 @@ async def amireallyalive(alive):
     chat = await alive.get_chat()
     await alive.delete()
     """ For .alive command, check if the bot is running.  """
-    await borg.send_message(chat, FRIDAY_IS_ALIVE) 
+    await borg.send_message(chat, JARVIS_IS_ALIVE) 
