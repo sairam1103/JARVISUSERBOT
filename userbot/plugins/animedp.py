11a61bd20861e09e77cd0da1232e2154aa3fe24f
sairam1103 <65078740+sairam1103@users.noreply.github.com>
2020-07-02 17:06:56 +0530

    Update animedp.py

--- userbot/plugins/animedp.py
+++ userbot/plugins/animedp.py
@@ -1,8 +1,4 @@
-#Made By @MarioDevs Keep Credits If You Are Goanna Kang This Lol
 
-#And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
-
-#Usage .actressdp Im Not Responsible For Any Ban caused By This
 
 import requests , re , random 
 
@@ -64,7 +60,7 @@ async def animepp():
 
 async def main(event):
 
-    await event.edit("**Starting Anime Profile Pic...\n\nDone !!! Check Your DP By @MarioDevs**")
+    await event.edit("**Starting Anime Profile Pic...\n\nDone !!! Check Your DP By @sai1103**")
 
     while True:
 
