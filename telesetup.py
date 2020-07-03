8eab46a7394af9714179957d9f6fed47c78bbcf7
sairam1103 <65078740+sairam1103@users.noreply.github.com>
2020-07-03 10:01:19 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -10,7 +10,7 @@ from telethon.sessions import StringSession
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details""")
+Create a new application, by entering the required details.STRING SESSION MADE BY @SIDDARTHA11""")
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
